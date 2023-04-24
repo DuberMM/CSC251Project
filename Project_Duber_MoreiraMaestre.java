ecda8d57d74b8e590c37ded641b5ae06797b2c3a
Duber Moreira Maestre <dmoreir1@email.cpcc.edu>
2023-04-23 20:46:56 -0400

    Implicitly Printing the ToString method and adding number of policy objs. created

--- Project_Duber_MoreiraMaestre.java
+++ Project_Duber_MoreiraMaestre.java
@@ -16,18 +16,22 @@ public class Project_Duber_MoreiraMaestre
       String smoker;
       double holderHeight;
       double holderWeight;
+      int policyCount = 0;
       int numSmokers = 0;
+      
       
       // Open File
       File file = new File("PolicyInformation.txt");
       
+      // Verifying the file exists
       if(!file.exists())
       {
          System.out.println("Unable to open file.");
          System.exit(0);
       }
       
-      Scanner inputPolicy = new Scanner(file);// Open the policy information file
+      // Open the policy information file
+      Scanner inputPolicy = new Scanner(file);
       
       // Array List To Store Objects
       ArrayList<Policy> policies = new ArrayList<Policy>();
@@ -46,40 +50,34 @@ public class Project_Duber_MoreiraMaestre
          holderHeight = inputPolicy.nextDouble();
          holderWeight = inputPolicy.nextDouble();
          
-         //make sure we haven't hit the end of the file before trying to skip the blank line
+         //To make sure we haven't hit the end of the file before trying to skip the blank line
          if(inputPolicy.hasNext())
             inputPolicy.nextLine();
          if(inputPolicy.hasNext())
-            inputPolicy.nextLine();   
+            inputPolicy.nextLine();
          
-         //create a Policy object and add it to our ArrayList
-         policies.add(new Policy(policyNumber, providerName, firstName, lastName, holderAge, smoker, holderHeight, holderWeight));
+         // Create Policy Holder Object
+         PolicyHolder pHolder = new PolicyHolder(firstName, lastName, holderAge, smoker, holderHeight, holderWeight);
+              
+         //Create a Policy object and add it to our ArrayList and also keep track of our count
+         policies.add(new Policy(policyNumber, providerName, policyCount, pHolder));
          
        }  
        
        // print out information for each policy object     
        for(Policy policy : policies) // in order: Class, new object : ArrayList
        {
+         
          //Displaying about the policy
+         System.out.println(policy); // implicitly calling the ToString method in Policy Class
          
-         System.out.print("\nPolicy Number: " + policy.getPolicyNumber());
-         System.out.print("\nProvider Name: " + policy.getProviderName());
-         System.out.print("\nPolicyholder's First Name: " + policy.getFirstName());
-         System.out.print("\nPolicyholder's Last Name: " + policy.getLastName());
-         System.out.print("\nPolicyholder's Age: " + policy.getAge());
-         System.out.print("\nPolicyholder's Smoking Status: " + policy.getSmoker());
-         System.out.printf("\nPolicyholder's Height: %,.1f inches\n", policy.getHeight());
-         System.out.printf("Policyholder's Weight: %,.1f pounds\n", policy.getWeight());
-         System.out.printf("Policyholder's BMI: %.2f\n", policy.bodyMassIndex());
-         System.out.printf("Policy Price: $%.2f\n", policy.policyCost());
-         System.out.println();
-         
-         if(policy.getSmoker().equalsIgnoreCase("smoker")) // Keep track number of smokers
+         if(policy.getPolicyHolder().getSmoker().equalsIgnoreCase("smoker")) // Keep track number of smokers
             numSmokers++;
-            
        }
        
       //print out the number of smokers and non-smokers
+      System.out.println("");
+      System.out.println("There were " + (policies.size()) + " Policy objects created.");
       System.out.println("The number of policies with a smoker is: " + numSmokers);
       System.out.println("The number of policies with a non-smoker is: " + (policies.size() - numSmokers));
       
